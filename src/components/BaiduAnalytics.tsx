0474e11c68062b0798f7e4274c1ebdd36d5b675d
saferhubert <yeyunfei698@foxmail.com>
2026-06-04 09:46:52 +0800

    Add files via upload

--- src/components/BaiduAnalytics.tsx
+++ src/components/BaiduAnalytics.tsx
@@ -2,10 +2,16 @@
 
 import { useEffect } from "react";
 
+declare global {
+  interface Window {
+    _hmt: any[];
+  }
+}
+
 export default function BaiduAnalytics() {
   useEffect(() => {
     if (typeof window !== "undefined" && !window._hmt) {
-      window._hmt = window._hmt || [];
+      window._hmt = [];
       const hm = document.createElement("script");
       hm.src = "https://hm.baidu.com/hm.js?d8015ff6d776cefc09d01bf7d33fc928";
       const s = document.getElementsByTagName("script")[0];
